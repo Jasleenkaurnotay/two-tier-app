d93acb7e71bc0bb5eb459288dcf187a70b96566f
JasK-Git <jasleen.notay@yahoo.com>
2026-04-26 20:15:38 +0530

    updating run.sh with recent commands

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-export DB_LINK="postgresql://postgres:password@localhost:5432/mydb"
-gunicorn app:app --bind 0.0.0.0:8000 &
+export DB_LINK='postgresql://postgres:app-db1!@app-db.cshaymiem82x.us-east-1.rds.amazonaws.com:5432/mydb'
+gunicorn run:app --bind 0.0.0.0:8000 &
 
 
 
