ef19a230cf550fd3652718d54e5fe7f57dd8657f
JasK-Git <jasleen.notay@yahoo.com>
2026-04-26 18:17:17 +0530

    Adding app code

---
+++ run.sh
@@ -0,0 +1,10 @@
+# in path day2/app
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+export DB_LINK="postgresql://postgres:password@localhost:5432/mydb"
+gunicorn app:app --bind 0.0.0.0:8000 &
+
+
+
